8e86ca032caa2358240e2f706e36ccc56735dc37
Deepit <minhvt223@gmail.com>
2024-04-06 19:42:27 +0700

    Add project files.

---
+++ Accounting Management/View/CustomerManage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Accounting_Management.View
+{
+    /// <summary>
+    /// Interaction logic for CustomerManage.xaml
+    /// </summary>
+    public partial class CustomerManage : Page
+    {
+        public CustomerManage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void AddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            Drawer.IsEnabled = true;
+            Drawer.Opacity = 1;
+            Drawer.Visibility = Visibility.Visible;
+            Drawer.Back.Click += Back_Click;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Drawer.IsEnabled = false;
+            Drawer.Visibility = Visibility.Hidden;
+            Drawer.Opacity = 0;
+        }
+    }
+}
---
+++ Accounting Management/View/StaffMenu.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Accounting_Management.View
+{
+    /// <summary>
+    /// Interaction logic for StaffMenu.xaml
+    /// </summary>
+    public partial class StaffMenu : Page
+    {
+        public StaffMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void DasboardClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void CustomerClick(object sender, MouseButtonEventArgs e)
+        {
+            FunctionUI.NavigationService.Navigate(new CustomerManage());
+        }
+        private void ProductClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void NoteClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void InvoiceClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void StaffClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private void AccountSystemClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+         private void LogClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
